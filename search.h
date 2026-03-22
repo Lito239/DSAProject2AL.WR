3889a3a5aaa7217ce54a011b61638c8f73857c8d
Wilfredo Rodriguez <wilfredo319401@gmail.com>
2026-03-22 19:00:40 -0400

    search.h created, more to be added

---
+++ search.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "CSV_Transfer.h"
+
+std::vector<CSVData> searchName(const std::vector<CSVData>& data, const std::string& key);
+std::vector<CSVData> filterByPopularity(const std::vector<CSVData>& data, double leastPopular);
+std::vector<CSVData) filterBySeasons (const std::vector<CSVData>& data, int leastNumSeasons);
